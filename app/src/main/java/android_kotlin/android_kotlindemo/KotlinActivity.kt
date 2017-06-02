4461aaeb810653e3e86fd14a6cab290836e67c3f
DESKTOP-DMBJUVJ\gavin <gavin6116@gmail.com>
2017-06-02 14:46:53 +0800

    Kotlin for Android

---
+++ app/src/main/java/android_kotlin/android_kotlindemo/KotlinActivity.kt
@@ -0,0 +1,12 @@
+package android_kotlin.android_kotlindemo
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class KotlinActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_kotlin)
+    }
+}
---
+++ app/src/main/java/android_kotlin/android_kotlindemo/User.kt
@@ -0,0 +1,10 @@
+package android_kotlin.android_kotlindemo
+
+/**
+ * Created by gavin
+ * Time 2017/6/2  14:41
+ * Email:molu_clown@163.com
+ */
+class User {
+
+}
